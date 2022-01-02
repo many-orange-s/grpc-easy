1421a597a56902c4ddf23f624983f563f211a912
many-orange-s <84865020+many-orange-s@users.noreply.github.com>
2022-01-02 21:12:48 +0800

    ADD:MTLS

--- client/main/main.go
+++ client/main/main.go
@@ -4,27 +4,45 @@ import (
 	pb "client/ecommerce"
 	"client/question"
 	"context"
+	"crypto/tls"
+	"crypto/x509"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	"io/ioutil"
 	"log"
 	"time"
 )
 
 const (
 	address  = "localhost:50051"
-	hostname = "localhost"
-	crtFile  = "D:\\go_project\\grpc-easy\\service\\server.crt"
+	hostname = "many.orange"
+	crtFile  = "D:\\go_project\\keys\\client.crt"
+	keyFile  = "D:\\go_project\\keys\\client.key"
+	caFile   = "D:\\go_project\\keys\\ca.crt"
 )
 
 func main() {
-	/*creds, err := credentials.NewClientTLSFromFile(crtFile, hostname)
+	cert, err := tls.LoadX509KeyPair(crtFile, keyFile)
 	if err != nil {
-		log.Fatalf("failed to load credentials :%v", err)
+		log.Fatalf("Could not load client keu pair : %s", err)
 	}
+	certPool := x509.NewCertPool()
+	ca, err := ioutil.ReadFile(caFile)
+	if err != nil {
+		log.Fatalf("could not read ca cer %s", err)
+	}
+	if ok := certPool.AppendCertsFromPEM(ca); !ok {
+		log.Fatalf("faild to append ca certs")
+	}
+
 	opts := []grpc.DialOption{
-		grpc.WithTransportCredentials(creds),
+		grpc.WithTransportCredentials(credentials.NewTLS(&tls.Config{
+			ServerName:   hostname,
+			Certificates: []tls.Certificate{cert},
+			RootCAs:      certPool,
+		})),
 	}
-	*/
-	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	conn, err := grpc.Dial(address, opts...)
 	if err != nil {
 		log.Fatalf("did not connect : %v", err)
 	}
